db6355a483b3e847af4a3461d8fa2ff63cfd3384
Piotr Rybka <piotr.rybka87@gmail.com>
2021-01-07 18:11:31 +0100

    Initial Commit

---
+++ R_Pogodynka/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  R_Pogodynka
+//
+//  Created by Piotr Rybka on 07/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ R_Pogodynka/R_PogodynkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  R_PogodynkaApp.swift
+//  R_Pogodynka
+//
+//  Created by Piotr Rybka on 07/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct R_PogodynkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
